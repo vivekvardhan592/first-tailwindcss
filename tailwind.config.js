3a2bea5486208d402e975502f56d71b0fec887b0
vivekvardhan592 <vivekvardhan592@gmail.com>
2025-06-21 18:01:50 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// const { plugin } = require("postcss");
+
+// tailwind.config.j;
+
+module.exports = {
+  darkMode: 'class',
+  content:['./index.html','./styles.css'],
+  theme:{
+    extend:{},
+  },
+  plugins: [],
+}
